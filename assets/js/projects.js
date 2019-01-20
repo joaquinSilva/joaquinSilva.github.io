31a888268b857ac9cded8d28bd7b5c64fd31fa3c
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2019-01-19 20:50:06 -0600

    updated project.js to include a new gif for the java contacts manager app and also updated the renderproject function on the project.js to render a different project card depending on whether the url is an empty string so that it will not render a list icon for the go to web app icon. Also updated my resume.

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,25 +7,35 @@ $(document).ready(function() {
 
     // array of project objects that allows the quick update of projects rendered to the page
     const projects = [
-        {'anchorTag': 'curiosities', 'title': "Cody's Curiosities", 'description': 'A craigslist - type java application that uses jsp files, servlets, and DAOs (data access objects) to allow users to create, read, update, and delete ads from a MySQL database. ', 'gifSrc': 'https://giphy.com/embed/2YjYpOl1eBy6I', 'url': 'https://www.google.com/', 'codeSrc': 'https://github.com/XanaduLister/java-adlister'},
-        {'anchorTag': 'contactsManager', 'title': 'Contacts Manager', 'description': 'A java application that uses conditional logic and loops to temporarily store data and respond to user input.', 'gifSrc': 'https://giphy.com/embed/2YjYpOl1eBy6I', 'url': 'https://www.google.com/', 'codeSrc': 'https://github.com/joaquin-juan/contacts-manager'},
-        {'anchorTag': 'moviesApp', 'title': 'Movies Application', 'description': 'A java application that uses recursive methods, loops, and JSON files to CRUD data.', 'gifSrc': 'https://giphy.com/embed/2YjYpOl1eBy6I', 'url': 'https://www.google.com/', 'codeSrc': 'https://github.com/kyla-joaquin/movies-application'},
-        {'anchorTag': 'weatherMaps', 'title': 'Weather Maps API', 'description': 'A javascript application that uses jQuery and AJAX requests to pull and render select data from a weather API.', 'gifSrc': 'https://giphy.com/embed/2YjYpOl1eBy6I', 'url': 'https://www.google.com/', 'codeSrc': 'https://www.google.com/'},
+        {'anchorTag': 'curiosities', 'title': "Cody's Curiosities", 'description': 'A craigslist - type java application that uses jsp files, servlets, and DAOs (data access objects) to allow users to create, read, update, and delete ads from a MySQL database. ', 'gifSrc': 'https://giphy.com/embed/2YjYpOl1eBy6I', 'url': '', 'codeSrc': 'https://github.com/XanaduLister/java-adlister'},
+        {'anchorTag': 'contactsManager', 'title': 'Contacts Manager', 'description': 'A java application that uses conditional logic and loops to temporarily store data and respond to user input.', 'gifSrc': 'https://giphy.com/embed/2Y9K4yuTyVoFLMOWGB', 'url': '', 'codeSrc': 'https://github.com/joaquin-juan/contacts-manager'},
+        {'anchorTag': 'moviesApp', 'title': 'Movies Application', 'description': 'A java application that uses recursive methods, loops, and JSON files to CRUD data.', 'gifSrc': 'https://giphy.com/embed/2YjYpOl1eBy6I', 'url': '', 'codeSrc': 'https://github.com/kyla-joaquin/movies-application'},
+        {'anchorTag': 'weatherMaps', 'title': 'Weather Maps API', 'description': 'A javascript application that uses jQuery and AJAX requests to pull and render select data from a weather API.', 'gifSrc': 'https://giphy.com/embed/2YjYpOl1eBy6I', 'url': '', 'codeSrc': 'https://www.google.com/'},
         {'anchorTag': 'coffee', 'title': 'Coffee Project', 'description': 'A front end application that uses DOM manipulation and javascript to dynamically render items based on a search bar input.', 'gifSrc': 'https://giphy.com/embed/iOdUipuPOuDpgFaLfx', 'url': 'https://stuart-joaquin.github.io/coffee-project/', 'codeSrc': 'https://github.com/stuart-joaquin/coffee-project'},
         {'anchorTag': 'codeup', 'title': 'Codeup Design', 'description': 'Exposure to front-end web design and responsive design concepts for different screen sizes utilizing bootstrap framework technology.', 'gifSrc': 'https://giphy.com/embed/clofJvzgEU1YogbyHF', 'url': 'https://steven-joaquin.github.io/codeup-design/', 'codeSrc': 'https://github.com/steven-joaquin/codeup-design'}
     ];
 
     // function that builds the html and css classes for a single project object
     const renderProject = ({ anchorTag, title, description, gifSrc, url, codeSrc }) => {
-        let html = `<hr id="${anchorTag}"><h3>${title}</h3><div style="width:100%;height:0;padding-bottom:54%;position:relative;"><iframe src="${gifSrc}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p>`;
-        html += `<p>${description}</p><ul class="icons"><li><a href="${url}" target="_blank" class="icon fa-link">`;
-        html += `</a></li><li>Go to Web App</li></ul><ul class="icons">`;
-        html += `<li><a href="${codeSrc}" target="_blank" class="icon fa-code">`;
-        html += `</a></li><li>View Source Code</li></ul><ul class="icons"><li>`;
-        html += `<a href="#projectList" class="icon fa-list-ul"></a></li>`;
-        html += `<li>Back to Project List</li></ul></div>`;
-        return html;
+
+            let html = `<hr id="${anchorTag}"><h3>${title}</h3><div style="width:100%;height:0;padding-bottom:54%;position:relative;"><iframe src="${gifSrc}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p>`;
+
+        if (url === '') {
+            html += `<p>${description}</p><ul class="icons">`;
+            html += `<li><a href="${codeSrc}" target="_blank" class="icon fa-code">`;
+            html += `</a></li><li>View Source Code</li></ul><ul class="icons"><li>`;
+            html += `<a href="#projectList" class="icon fa-list-ul"></a></li>`;
+            html += `<li>Back to Project List</li></ul></div>`;
+        } else {
+            html += `<p>${description}</p><ul class="icons"><li><a href="${url}" target="_blank" class="icon fa-link">`;
+            html += `</a></li><li>Go to Web App</li></ul><ul class="icons">`;
+            html += `<li><a href="${codeSrc}" target="_blank" class="icon fa-code">`;
+            html += `</a></li><li>View Source Code</li></ul><ul class="icons"><li>`;
+            html += `<a href="#projectList" class="icon fa-list-ul"></a></li>`;
+            html += `<li>Back to Project List</li></ul></div>`;
+        } return html;
     };
+
 
     // function that builds the html and css for a single project list item for the nav links to projects
     const renderListItem = ({ anchorTag, title }) => {
